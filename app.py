36b2d40b1dc07fda838cada76ee78a0420ecb93f
zhangjiaheng001 <565293676@qq.com>
2024-04-12 06:41:52 +0000

    内容修整

--- app.py
+++ app.py
@@ -8,9 +8,16 @@ from timeit import default_timer as timer
 from typing import Tuple, Dict
 
 # Setup class names
+
+with open("class_names.txt", "r") as f: # reading them in from class_names.txt
+    class_names_0= [food_name.strip() for food_name in  f.readlines()]
+
 with open("class_names_chinese.txt", "r") as f: # reading them in from class_names.txt
-    class_names = [food_name.strip() for food_name in  f.readlines()]
-    
+    class_names_1 = [food_name.strip() for food_name in  f.readlines()]
+
+class_names=[]
+for _ in range(len(class_names_0)):
+    class_names.append(class_names_0[_]+', '+class_names_1[_])    
 ### 2. Model and transforms preparation ###    
 
 # Create model
@@ -73,11 +80,15 @@ def predict(img) -> Tuple[Dict, float]:
 
 # Create title, description and article strings
 #title = "FoodVision Big 🍔👁"
-description = "训练集使用的food101数据集，其中包含101种不同类型食品，\
-    [原项目](https://www.learnpytorch.io/09_pytorch_model_deployment/)在测试集上的精度大约为60%，\
-    我这边主要简单的替换了其中的分类模型，使得精度提到80%以上,同时也进了中文化处理，\
-    注意此分类器只包含了101个品种的食物，如披萨，饺子，炸薯条，炒饭，巧克力慕斯等等，[详细品类详见此处](https://huggingface.co/spaces/john000z/foodvision_assum/blob/main/class_names_chinese.txt)."
-article = "Created at [09. PyTorch Model Deployment]."
+description = "分类器使用的训练数据的是food101数据集，其中包含101种不同类型食品，\
+    如披萨，饺子，炸薯条，炒饭，巧克力慕斯等，\
+    每种类别各100张训练图片\
+    [详细品类列表详见此处](https://huggingface.co/spaces/john000z/foodvision_assum/blob/main/class_names_chinese.txt). \
+    大家可以根据**品类列表**从网上寻找对应的图片进行识别"
+    
+    
+article = "[原项目](https://www.learnpytorch.io/09_pytorch_model_deployment/)在测试集上的精度大约为60%，\
+    我这边替换了其中的模型并重新训练了数据，使得精度提到80%以上，同时也对界面进了中文化处理"
 
 title = "食品分类器 🍔👁"
 
